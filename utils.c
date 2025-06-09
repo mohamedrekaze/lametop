f183871a55f85d6e660112aa8552cddac4565ae4
mohamedrekaze <mohamedrekaze@gmail.com>
2025-06-09 20:19:35 +0100

    lametop - opendir / readdir first impression[1;5C

--- utils.c
+++ utils.c
@@ -5,12 +5,13 @@
 #include <limits.h>
 
 void open_and_print_proc() {
-	DIR *dir = opendir("/home/mohamed/dotfiles/");
-	struct dirent *s = readdir(dir);
-	printf("%s\n", s->d_name);
+	DIR *dir = opendir(".");
+	struct dirent *s;
+
+	while((s = readdir(dir) )!= NULL) {
+		printf("%s\n", s->d_name);
+	}
 }
-
-
 
 int main() {
 	open_and_print_proc();
