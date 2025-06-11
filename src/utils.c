885bf64cbd2fbc45d13c544814e24615e7dffd61
mohamedrekaze <mohamedrekaze@gmail.com>
2025-06-11 20:55:39 +0100

    lametop - path handling

--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,7 @@
 #include "lametop.h"
 #include <ctype.h>
 #include <dirent.h>
+#include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,14 +13,22 @@ int is_num(char *file_name) {
 	int i = 0;
 	while(file_name[i]) {
 		if(!isdigit(file_name[i]))
+			return 0;
 		i++;
 	}
+	return 1;
 }
 
-void check_pid(struct dirent *file) {
+void check_stats() {
 	
 }
 
-void file_check(struct dirent *file) {
-	if((file->d_name))
+int pid_file_check(struct dirent *file) {
+	char *path;
+	char name[NAME_MAX + 10];
+	if(is_num(file->d_name)) {
+		printf("it worked this is the path:\t%s\n", name);
+	}
+	strcpy(name, file->d_name);
+	snprintf(name, sizeof(name), "/proc/%s", file->d_name);
 }
